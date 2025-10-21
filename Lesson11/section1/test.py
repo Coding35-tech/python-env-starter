0ac0335d7a080eb2c6b89cd97540831768990ae4
Coding35-tech <aya.elmoussaoui317@gmail.com>
2025-10-21 22:33:22 +0000

    lesson 11 edits

--- Lesson11/section1/test.py
+++ Lesson11/section1/test.py
@@ -5,5 +5,10 @@ class TestIsOdd(unittest.TestCase):
     def test_odd_num(self):
         self.assertTrue(is_odd(3))
 
+    def test_even_num(self):
+        self.assertTrue(is_odd(2))
+
+    def test_zero(self):
+        self.assertTrue(is_odd(0))
 if __name__ == '__main__':
     unittest.main()
